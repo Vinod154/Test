ec260ec12438c98e3fa7acb1c4a171c5adf5f31c
Vinod154 <gaikwadvinod154@gmail.com>
2020-08-12 18:44:29 +0530

    home controller changes

--- Test/Controllers/HomeController.cs
+++ Test/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace Test.Controllers
 
             return View();
         }
+
+        public ActionResult Test()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
